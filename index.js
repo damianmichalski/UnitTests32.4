4415182f15524bba26abfe4b93bb08cb4548288b
DamianM <damian.michalski@kramp.com>
2019-03-20 12:26:20 +0100

    Unit Tests - update after feedback v2

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+const dayInSeconds = 86400;
+const dayInHours = 3600;
+const minutes = 60;
+
 const formatDate = (timeInSeconds) => {
-    const dayInSeconds = 86400;
     const day = Math.floor(timeInSeconds / dayInSeconds);
     let rest = timeInSeconds % dayInSeconds;
-    const dayInHours = 3600;
     const hours = Math.floor(rest / dayInHours);
     rest = rest % dayInHours;
-    const dayInMinutes = 60;
-    const min =  Math.floor(rest / dayInMinutes);
-    const sec = Math.floor(rest % dayInMinutes);
+    const min =  Math.floor(rest / minutes);
+    const sec = Math.floor(rest % minutes);
     let time = '';
-    if (!time.length) {
+    if (!timeInSeconds) {
         return '0s';
     }
     if (day > 0) {
@@ -20,12 +21,12 @@ const formatDate = (timeInSeconds) => {
     }
     if (min > 0) {
         time += min + 'm ';
-    } 
+    }
     if (sec > 0) {
         time += sec + 's';
-    } 
+    }
 
     return time.trim();
 }
-    
+
 module.exports = formatDate;
